411c743ee51418d6659a720f8af4846089eadea7
Rafael Ravedutti <rafael@laura-br.com>
2018-10-17 22:53:07 +0200

    Add exercises with Canvas utilities and SVG
    Signed-off-by: Rafael Ravedutti <rafael@laura-br.com>

--- Exercise1/Basic/Basic2.js
+++ Exercise1/Basic/Basic2.js
@@ -1,10 +1,19 @@
 function drawArcCircle(canvas) {
     var context = canvas.getContext("2d");
 
-    //TODO 1.2)       Use the arc() function to
-    //                rasterize the two circles
-    //                from Task 1.1.
+    context.beginPath();
+    context.arc(60, 60, 50, 0, 2 * Math.PI);
+    context.fillStyle = "#00ff00";
+    context.fill();
 
+    context.beginPath();
+    context.arc(140, 140, 50, 0, 2 * Math.PI);
+    context.fillStyle = "#00ff00";
+    context.fill();
 
-
+    context.beginPath();
+    context.arc(140, 140, 50, 0, 2 * Math.PI);
+    context.strokeStyle = "#007f00";
+    context.lineWidth = 10;
+    context.stroke();
 }
