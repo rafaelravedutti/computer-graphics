bef2b33e847fa7f76926a031bb10e0b9ee33e633
Lucas B. Cunha <lucas.braz.cunha@gmail.com>
2018-10-28 15:01:47 -0300

    basic1 working but needs correction on the number of iterations
    Signed-off-by: Lucas B. Cunha <lucas.braz.cunha@gmail.com>

--- Exercise2/Basic/Basic1.js
+++ Exercise2/Basic/Basic1.js
@@ -67,26 +67,40 @@ function bresenham(image, line) {
     //              between the start point and
     //              the end point. To make things
     //              easier, there are some comments
-    //              on what to do next: 
+    //              on what to do next:
 
     // compute deltas and update directions
 
 
-
     // set initial coordinates
+    var deltax = Math.abs(x1 - x0), movementx = x0 < x1 ? 1 : -1;
+    var deltay = Math.abs(y1 - y0), movementy = y0 < y1 ? 1 : -1;
+    var err = (deltax > deltay ? deltax : -deltay)/2;
+    var e2;
 
 
+    // start loop to set nPixels
+    var intx0 = Math.floor(x0);
+    var intx1 = Math.floor(x1);
+    var inty0 = Math.floor(y0);
+    var inty1 = Math.floor(y1);
 
-    // start loop to set nPixels 
-    var nPixels = 0; // think about how many pixels need to be set - zero is not correct ;)
+     var nPixels = Math.abs(x1 - x0) + Math.abs(y1 - y0)
+    // Math.floor(Math.sqrt((intx1 - intx0)*(intx1 - intx0) + (inty1 - inty0)*(inty1 - inty0)));
+    // console.log(nPixels);
+    // console.log(line.startPoint)
+    // console.log(line.endPoint);
+
     for (var i = 0; i < nPixels; ++i) {
         // set pixel using the helper function setPixelS()
-
+        setPixelS(image, new Point(x0,y0), new Color(0, 0, 0), pixelScale)
 
         // update error
-
+        e2 = err;
 
         // update coordinates depending on the error
+        if (e2 > -deltax) { err -= deltay; x0 += movementx; }
+        if (e2 < deltay) { err += deltax; y0 += movementy; }
 
 
     }
