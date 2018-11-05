4acb93e62387227b87f5d69ac48b149ee06704de
Rafael Ravedutti <rafael@laura-br.com>
2018-11-05 11:50:44 +0100

    Fix indentation
    Signed-off-by: Rafael Ravedutti <rafael@laura-br.com>

--- Exercise3/Advanced/shader_water.fs
+++ Exercise3/Advanced/shader_water.fs
@@ -9,7 +9,7 @@ void main(void)
   vec3 dark_blue = vec3(0, 0, 0.3);
   vec3 light_blue = vec3(0.4, 0.4, 1.0);
 
-	gl_FragColor = vec4(mix(
+  gl_FragColor = vec4(mix(
     dark_blue,
     light_blue,
     position.y + 0.5
