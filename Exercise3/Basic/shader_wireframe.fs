80620f9d0a3b0070f39438668e187ab993b11ee7
Lucas B. Cunha <lucas.braz.cunha@gmail.com>
2018-11-08 00:00:26 +0100

    Initial commit for Basic2
    Signed-off-by: Lucas B. Cunha <lucas.braz.cunha@gmail.com>

--- Exercise3/Basic/shader_wireframe.fs
+++ Exercise3/Basic/shader_wireframe.fs
@@ -1,11 +1,11 @@
 precision mediump float;
 
 // TODO 3.2a)	Define the varying variable again
-//				using the same name to enable 
+//				using the same name to enable
 //				communication between vertex and
 //				fragment shader.
 
-
+varying vec3 colorVertex;
 
 void main(void)
 {
@@ -14,15 +14,15 @@ void main(void)
 
 	// TODO 3.2a)	Give each pixel the interpolated
 	//				triangle color.
-	gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+	gl_FragColor = vec4(colorVertex, 1.0);
 
 
-	
+
 	// TODO 3.2b)	Use the color as barycentric coordinates
-	//				and discard all pixels not considered 
-	//				edges (farther away from an edge than 
+	//				and discard all pixels not considered
+	//				edges (farther away from an edge than
 	//				epsilon). Use the GLSL mechanism 'discard'.
-	
+
 
 
 }
--- Exercise3/Basic/shader_wireframe.vs
+++ Exercise3/Basic/shader_wireframe.vs
@@ -4,12 +4,12 @@ attribute vec2 vVertex;
 // TODO 3.2a)	Create an attribute describing the
 //				color of a vertex.
 
+attribute vec3 colorAtr;
 
-
-// TODO 3.2a)	Create an varying variable to 
+// TODO 3.2a)	Create an varying variable to
 //				pass the interpolated color to
 //				the fragment shader.
-
+varying vec3 colorVertex;
 
 
 void main(void)
@@ -18,6 +18,7 @@ void main(void)
 
 	// TODO 3.2a)	Assign the color of the vertex
 	//				to the varying variable.
+	colorVertex = colorAtr;
 
 
 
