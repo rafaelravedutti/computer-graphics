ee19db4d209cd9fb3287882ddefc4bc68ffbafc0
Lucas B. Cunha <lucas.braz.cunha@gmail.com>
2018-11-13 17:25:07 +0100

    Done exercise a and b, still working on c and d
    Signed-off-by: Lucas B. Cunha <lucas.braz.cunha@gmail.com>

--- Exercise4/Basic/Basic1.js
+++ Exercise4/Basic/Basic1.js
@@ -62,32 +62,50 @@ var Basic1_1 = function () {
 
     function Rotation(alpha) {
 
-        // TODO 4.1a)	Implement a linear transformation 
-        //			    performing a rotation by the angle 
+        // TODO 4.1a)	Implement a linear transformation
+        //			    performing a rotation by the angle
         //			    alpha and replace the following line
         //			    by the appropriate code.
-        return new LinearTransformation([1, 0, 0, 1]);
+        //  [cos(alpha)   -sin]    [1]
+        //  [sin         cos] *  [1]
+        let a1 = Math.cos(alpha);
+        let a2 = -Math.sin(alpha);
+        let a3 = Math.sin(alpha);
+        let a4 = Math.cos(alpha);
+        return new LinearTransformation([a1, a2, a3, a4]);
 
     }
 
     function Scaling(scale) {
 
-        // TODO 4.1a)	Implement a linear transformation 
-        //			    performing an isotropic scaling by 
+        // TODO 4.1a)	Implement a linear transformation
+        //			    performing an isotropic scaling by
         //			    the scaling factor scale and replace
-        //			    the following line by the appropriate 
+        //			    the following line by the appropriate
         //			    code.
-        return new LinearTransformation([1, 0, 0, 1]);
+        //  [scale   0]    [1]
+        //  [0   scale] *  [1]
+        let a1 = scale;
+        let a2 = 0;
+        let a3 = 0;
+        let a4 = scale;
+        return new LinearTransformation([a1, a2, a3, a4]);
 
     }
 
     function ShearingX(shearX) {
 
-        // TODO 4.1a)	Implement a linear transformation 
-        //			    performing a shear along the x axis. 
+        // TODO 4.1a)	Implement a linear transformation
+        //			    performing a shear along the x axis.
         //			    Replace the following line by the
         //			    appropriate code.
-        return new LinearTransformation([1, 0, 0, 1]);
+        //  [1   shearX]
+        //  [0        1]
+        let a1 = 1;
+        let a2 = shearX;
+        let a3 = 0;
+        let a4 = 1;
+        return new LinearTransformation([a1, a2, a3, a4]);
 
     }
 
@@ -130,19 +148,31 @@ var Basic1_2 = function () {
 
     function ShearingX(shearX) {
 
-        // TODO 4.1b)	Implement a linear transformation 
-        //			    performing a shear along the x axis. 
+        // TODO 4.1b)	Implement a linear transformation
+        //			    performing a shear along the x axis.
         //              Replace the following code.
-        return new LinearTransformation([1, 0, 0, 1]);
+        //  [1   shearX]
+        //  [0        1]
+        let a1 = 1;
+        let a2 = -Math.tan(shearX/2.0);
+        let a3 = 0;
+        let a4 = 1;
+        return new LinearTransformation([a1, a2, a3, a4]);
 
     }
 
     function ShearingY(shearY) {
 
-        // TODO 4.1b)	Implement a linear transformation 
-        //			    performing a shear along the y axis. 
+        // TODO 4.1b)	Implement a linear transformation
+        //			    performing a shear along the y axis.
         //              Replace the following code.
-        return new LinearTransformation([1, 0, 0, 1]);
+        //  [1       0]
+        //  [shearY  1]
+        let a1 = 1;
+        let a2 = 0;
+        let a3 = Math.sin(shearY);
+        let a4 = 1;
+        return new LinearTransformation([a1, a2, a3, a4]);
 
     }
 
@@ -162,30 +192,39 @@ var Basic1_2 = function () {
             context.fillText("1. shearing", 225, 140);
 
             // TODO 4.1b)	Instead of just copying the corner points
-            //			    of triangle, call shearingX with the 
+            //			    of triangle, call shearingX with the
             //			    corresponding parameters!
             //              Use ApplyLinearTransformation() to transform the corner points.
-            var triangle1 = new Triangle(triangle.a, triangle.b, triangle.c);
+            var shearingX = ShearingX(alpha)
+            var triangle1 = new Triangle(ApplyLinearTransformation(shearingX, triangle.a),
+                                                   ApplyLinearTransformation(shearingX, triangle.b),
+                                                   ApplyLinearTransformation(shearingX, triangle.c));
 
             RenderTriangle(context, new Viewport(150, 150, 150, 0), triangle1);
 
             context.fillText("2. shearing", 375, 140);
 
             // TODO 4.1b)	Instead of just copying the corner points
-            //			    of triangle1, call shearingY with the 
+            //			    of triangle1, call shearingY with the
             //			    corresponding parameters!
             //              Use ApplyLinearTransformation() to transform the corner points.
-            var triangle2 = new Triangle(triangle1.a, triangle1.b, triangle1.c);
+            var shearingY = ShearingY(alpha)
+            var triangle2 = new Triangle(ApplyLinearTransformation(shearingY, triangle1.a),
+                                                   ApplyLinearTransformation(shearingY, triangle1.b),
+                                                   ApplyLinearTransformation(shearingY, triangle1.c));
 
             RenderTriangle(context, new Viewport(150, 150, 300, 0), triangle2);
 
             context.fillText("3. shearing", 525, 140);
 
             // TODO 4.1b)	Instead of just copying the corner points
-            //			    of triangle2, call shearingX with the 
+            //			    of triangle2, call shearingX with the
             //			    corresponding parameters!
             //              Use ApplyLinearTransformation() to transform the corner points.
-            var triangle3 = new Triangle(triangle2.a, triangle2.b, triangle2.c);
+            shearingX = ShearingX(alpha)
+            var triangle3 = new Triangle(ApplyLinearTransformation(shearingX, triangle2.a),
+                                                   ApplyLinearTransformation(shearingX, triangle2.b),
+                                                   ApplyLinearTransformation(shearingX, triangle2.c));
 
 
             RenderTriangle(context, new Viewport(150, 150, 450, 0), triangle3);
@@ -195,13 +234,38 @@ var Basic1_2 = function () {
 
 var Basic1_3 = function () {
 
+  function multiplyMatrix(out, a, b) {
+	    let a0 = a[0], a1 = a[1], a2 = a[2], a3 = a[3],
+	        b0 = b[0], b1 = b[1], b2 = b[2], b3 = b[3];
+	    out[0] = a0 * b0 + a1 * b2;
+	    out[1] = a0 * b1 + a1 * b3;
+	    out[2] = a2 * b0 + a3 * b2;
+	    out[3] = a2 * b1 + a3 * b3;
+	    return out;
+	}
+
+  function sumArray(out, a, b){
+      let a0 = a[0], a1 = a[1];
+      let b0 = b[0], b1 = b[1];
+      out[0] = a0 + b0;
+      out[1] = a1 + b1;
+      return out;
+  }
+
+
+  // Not working correctly!!!
+
     function CompositeAffineTransformations(affineTransf2, affineTransf1) {
 
         // TODO 4.1c)	Replace the following line by creation
         //			    of the affine transformation equivalent
         //			    to the composition of affineTransf1 and
         //			    affineTransf2.
-        return new AffineTransformation([1, 0, 0, 1], [0, 0]);
+
+        let mult = [4], sumT = [2];
+        multiplyMatrix(mult, affineTransf1.A, affineTransf2.A);
+        sumArray(sumT, affineTransf1.t, affineTransf2.t);
+        return new AffineTransformation(mult, sumT);
 
     }
 
@@ -240,8 +304,8 @@ var Basic1_4 = function () {
 
         // Note: The original triangle has the fixed vertices (0, 0), (1, 0) and (0, 1).
 
-        // TODO 4.1d) 	Compute the affine transformation that 
-        //			    transforms the triangle to the target 
+        // TODO 4.1d) 	Compute the affine transformation that
+        //			    transforms the triangle to the target
         //			    triangle triangleTarget!
         return new AffineTransformation([1.0, 0.0, 0.0, 1.0], [0.0, 0.0]);
 
