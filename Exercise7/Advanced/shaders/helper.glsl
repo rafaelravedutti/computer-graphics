12218630382643cd8129249f8df8c62862ff0050
Rafael Ravedutti <rafaelravedutti@gmail.com>
2018-12-03 00:18:54 +0100

    Coordinate Conversions
    Signed-off-by: Rafael Ravedutti <rafaelravedutti@gmail.com>

--- Exercise7/Advanced/shaders/helper.glsl
+++ Exercise7/Advanced/shaders/helper.glsl
@@ -5,6 +5,9 @@
     float theta = 0;
     float phi = 0;
 
+    theta = arctan(n.z / n.x);
+    phi = arccos(n.y);
+
     return vec2(theta,phi);
 }
 
@@ -12,11 +15,15 @@ vec3 sphericalToCartesian(vec2 a)
 {
     float theta = a.x;
     float phi = a.y;
+    vec3 result(0);
 
     // TODO 7.4 a)
     // Convert spherical coordinates to cartesian coordinates.
-    return vec3(0,0,0);
+    result.x = sin(phi) * cos(theta);
+    result.y = cos(phi);
+    result.z = sin(phi) * sin(theta);
 
+    return result;
 }
 
 
@@ -25,12 +32,26 @@ vec2 sphericalToTexture(vec2 a)
     const float PI = 3.14159265;
     float theta = a.x; //in range [-PI,PI]
     float phi = a.y; // in range [0,PI]
+    vec2 uv(0);
 
     // TODO 7.4 a)
     // Compute texture coordinates from spherical coordinates.
 	// Do not forget to mirror both coordinates to have the north pole at the top 
 	// and France located west of Germany! ;)
-    return vec2(0,0);
+    uv.x = (theta + PI) / (2.0 * PI) + 0.5;
+    uv.y = phi / PI + 0.5;
 
+    uv -= 2.0 * floor(uv / 2.0);
+
+    if(uv.x > 1.0) {
+      uv.x = 2.0 - uv.x;
+    }
+
+    if(uv.y > 1.0) {
+      uv.y = 1.0 - uv.y;
+    }
+
+    return uv;
 }
+
 
